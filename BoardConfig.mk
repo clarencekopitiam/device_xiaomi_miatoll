1ee82198edc835583033682c4e25e4db2a6dde21
Dhina17 <dhinalogu@gmail.com>
2025-02-09 17:21:53 +0100

    miatoll: Implement torch light control
    [RealJohnGalt:] modify for oneplus usage, add toggle switch
    [cyberknight777]: Adapt nodes and SEPolicy
    [clarencelol]: Adapt minor changes for miatoll
    
    Change-Id: I0b86fbeefbe3b951c5b9827a06c066dd71ada002
    
    Co-authored-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: clarencelol <clarencekuiek@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -42,6 +42,7 @@ TARGET_NO_BOOTLOADER := true
 
 # Camera
 TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED := true
+TARGET_CAMERA_SERVICE_EXT_LIB := //$(DEVICE_PATH):libcameraservice_extension.miatoll
 TARGET_DISABLED_UBWC := true
 
 # Display
